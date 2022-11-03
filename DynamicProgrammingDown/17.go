ff4d2140131c383a46148badf499a9e485c622fc
andelu <andelu1996@163.com>
2022-11-03 09:38:00 +0800

    add some fiels

--- DynamicProgrammingDown/17.go
+++ DynamicProgrammingDown/17.go
@@ -25,4 +25,27 @@ answer:
 
 func longestPalindromeSubseq(s string) int {
 	// dp数组和初始化
+	dp := make([][]int, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]int, len(s))
+		dp[i][i] = 1
+	}
+	// 遍历
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][len(s)-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
