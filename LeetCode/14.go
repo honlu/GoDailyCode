357ecd4ab7e7faabce6f06f91ee3347d22b41448
andelu <andelu1996@163.com>
2022-12-07 13:17:34 +0800

    add 14.go

---
+++ LeetCode/14.go
@@ -0,0 +1,33 @@
+/*
+14. 最长公共前缀
+2022-12-7
+link: https://leetcode.cn/problems/longest-common-prefix/
+question:
+	编写一个函数来查找字符串数组中的最长公共前缀。
+	如果不存在公共前缀，返回空字符串 ""。
+answer:
+	遍历
+*/
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 1 { // 特殊情况 ["a"]
+		return strs[0]
+	}
+	var res string
+	temp := 0
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i < len(strs[j]) && strs[j][i] == strs[0][i] {
+				if j == len(strs)-1 { // 特殊情况 ["flower","flower","flower","flower"]
+					temp = i + 1
+				}
+				continue
+			} else {
+				temp = i
+				goto Loop
+			}
+		}
+	}
+Loop:
+	res = strs[0][:temp]
+	return res
+}
