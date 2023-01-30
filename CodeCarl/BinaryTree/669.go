0856e877acd46df0e2139ef598fa27bf31d0ae6f
andelu <andelu1996@163.com>
2023-01-30 13:59:26 +0800

    update 669.go

--- CodeCarl/BinaryTree/669.go
+++ CodeCarl/BinaryTree/669.go
@@ -16,7 +16,7 @@ idea:
 		将root移动到[L, R] 范围内，注意是左闭右闭区间
 		剪枝左子树
 		剪枝右子树
-	要注意考虑小于low的右子树。
+	要注意考虑小于low节点的右子树可能存在大于low的存在！！！
 */
 
 // 递归实现-剪枝二叉搜索树（前序遍历）
@@ -34,5 +34,46 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
 	}
 	root.Left = trimBST(root.Left, low, high)
 	root.Right = trimBST(root.Right, low, high)
+	return root
+}
+
+// 迭代实现
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	// 处理头节点，让root移动到[L,R]范围内，注意是左闭右闭
+	for root != nil && (root.Val < low || root.Val > high) {
+		if root.Val < low {
+			root = root.Right // root向右走
+		} else {
+			root = root.Left // root向左走
+		}
+	}
+	// 此时root已经在[L,R]范围内，处理左孩子元素小于low的情况
+	cur := root
+	for cur != nil {
+		for cur.Left != nil && cur.Left.Val < low {
+			cur.Left = cur.Left.Right
+		}
+		cur = cur.Left
+	}
+	// 处理右孩子大于high的情况
+	cur = root
+	for cur != nil {
+		for cur.Right != nil && cur.Right.Val > high {
+			cur.Right = cur.Right.Left
+		}
+		cur = cur.Right
+	}
+
 	return root
 }
