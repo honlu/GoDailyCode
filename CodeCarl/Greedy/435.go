be558b326f0321c158c88b9e16d1b042e0a3e37a
andelu <andelu1996@163.com>
2023-02-19 22:23:48 +0800

    update 435.go

--- CodeCarl/Greedy/435.go
+++ CodeCarl/Greedy/435.go
@@ -1,7 +1,9 @@
 /*
-14、无重叠部分
+14
+435. 无重叠部分
 2022-10-13
-link: 435-https://leetcode.cn/problems/non-overlapping-intervals/
+2023-2-19 updated by lu
+link: https://leetcode.cn/problems/non-overlapping-intervals/
 question:
 	给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。
 	返回 需要移除区间的最小数量，使剩余区间互不重叠
@@ -30,4 +32,28 @@ func eraseOverlapIntervals(intervals [][]int) int {
 		}
 	}
 	return res
+}
+
+// 保存不重叠的区间
+func eraseOverlapIntervals(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
+	// 排序-左边界排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0] || (intervals[i][0] == intervals[j][0] && intervals[i][1] < intervals[j][1])
+	})
+	var temp [][]int // 保存不重叠的区间
+	temp = append(temp, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if temp[len(temp)-1][1] <= intervals[i][0] {
+			temp = append(temp, intervals[i])
+		} else if temp[len(temp)-1][1] > intervals[i][0] {
+			if temp[len(temp)-1][1] > intervals[i][1] { // 这里是关键
+				temp = temp[:len(temp)-1]
+				temp = append(temp, intervals[i])
+			}
+		}
+	}
+	return len(intervals) - len(temp)
 }
