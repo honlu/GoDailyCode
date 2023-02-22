0a35815280972b0cde4696d7ebc69b7f4bfc6587
andelu <andelu1996@163.com>
2023-02-22 10:31:59 +0800

    update 714 and 968.go

--- CodeCarl/Greedy/714.go
+++ CodeCarl/Greedy/714.go
@@ -1,6 +1,8 @@
 /*
-18. 买卖股票地最佳时机含手续费
+18
+714. 买卖股票地最佳时机含手续费
 2022-10-13
+2023-2-22 updated by lu
 link:714-https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 question:
 	给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；
@@ -12,29 +14,31 @@ answer:
 	贪心策略，就是最低值买，最高值（如果算上手续费还盈利）就卖。
 
 	做收获利润操作的时候其实有三种情况：
-	情况一：收获利润的这一天并不是收获利润区间里的最后一天（不是真正的卖出，相当于持有股票），所以后面要继续收获利润。
+	情况一：收获利润的这一天并不是收获利润区间里的最后一天（不是真正的卖出，相当于持有股票），所以后面要继续收获利润。[这个实现是关键！]
 	情况二：前一天是收获利润区间里的最后一天（相当于真正的卖出了），今天要重新记录最小价格了。
 	情况三：不作操作，保持原有状态（买入，卖出，不买不卖）[代码实现的部分可以忽略]
 */
-
+// 贪心思路
 func maxProfit(prices []int, fee int) int {
 	res := 0
-	min := prices[0] // 记录最低价格
+	minPrice := prices[0] // 记录最低价格
 	for i := 1; i < len(prices); i++ {
 		// 情况2，相当于买入
-		if prices[i] < min {
-			min = prices[i] // 此时买入
+		if prices[i] < minPrice {
+			minPrice = prices[i] // 此时买入
 		}
 		// 情况3，保持原有状态（因为此时买不便宜，卖则亏本）
-		if prices[i] >= min && prices[i] <= min+fee {
+		if prices[i] >= minPrice && prices[i] <= minPrice+fee {
 			continue
 		}
-		// 计算利润，可能有多次计算利润，最后一次计算利润才是真正意义的卖出
+		// 情况1：计算利润，可能有多次计算利润，最后一次计算利润才是真正意义的卖出
 		// 这里有些绕，很关键！
-		if prices[i] > min+fee {
-			res += prices[i] - min - fee
-			min = prices[i] - fee // 情况1，这一步很关键！不好懂哦！要多思考和举例
+		if prices[i] > minPrice+fee {
+			res += prices[i] - minPrice - fee
+			minPrice = prices[i] - fee // 这一步很关键！要保证遇到真正的高点才算卖出
 		}
 	}
 	return res
 }
+
+// 动态规划实现代码在DynamicProgram章节实现
--- CodeCarl/Greedy/968.go
+++ CodeCarl/Greedy/968.go
@@ -1,6 +1,8 @@
 /*
-19、监控二叉树
+19
+968. 监控二叉树
 2022-10-13
+2023-2-22 updated by lu
 link:968-https://leetcode.cn/problems/binary-tree-cameras/
 question:
 	给定一个二叉树，我们在树的节点上安装摄像头。
@@ -25,6 +27,45 @@ answer:
  * }
  */
 
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minCameraCover(root *TreeNode) int {
+	res := 0 // 安装摄像头的数目
+
+	var backTrack func(root *TreeNode) int // 有返回值表示节点的状态：0表示节点没有被覆盖，1表示当前节点是摄像头，2表示被摄像头覆盖
+	backTrack = func(root *TreeNode) int {
+		if root == nil { // 空节点默认表示被覆盖
+			return 2
+		}
+
+		left := backTrack(root.Left)
+		right := backTrack(root.Right)
+
+		if left == 2 && right == 2 { // 左右子节点都被覆盖
+			return 0 // 父节点无覆盖状态
+		} else if left == 0 || right == 0 { // 左右子节点有一个没有被覆盖，父节点就要设置摄像头，使其被覆盖监控
+			res++
+			return 1
+		} else { // 左右子节点有一个安装摄像头
+			return 2 // 则当前节点被覆盖
+		}
+
+		return -1 // 只是为了返回值，不会走到这里
+	}
+
+	if backTrack(root) == 0 { // 如果根节点没有被覆盖
+		res++
+	}
+	return res
+}
+
+// 函数提出来写
 func minCameraCover(root *TreeNode) int {
 	res := 0
 	// 情况4-root根节点没有被覆盖
@@ -34,15 +75,15 @@ func minCameraCover(root *TreeNode) int {
 	return res
 }
 
-// 后序遍历
+// 后序遍历：注意有返回值，表示节点的状态
 func backTrack(root *TreeNode, res *int) int {
 	// 空节点，该节点有覆盖
 	if root == nil {
 		return 2
 	}
 
-	left := backTrack(root.Left, res)   // 先左节点
-	right := backTrack(root.Right, res) // 然后右节点
+	left := backTrack(root.Left, res)   // 先左节点的状态
+	right := backTrack(root.Right, res) // 然后右节点的状态
 	// 最后中间节点-后序遍历
 	// 情况1-左右节点都有覆盖
 	if left == 2 && right == 2 {
