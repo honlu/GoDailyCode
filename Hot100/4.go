7f1232a0cd8212301257948ef6fc45c78976f54c
andelu <andelu1996@163.com>
2025-03-28 22:09:20 +0800

    Feat(hot100):add 5-盛更多的水

--- Hot100/4.go
+++ Hot100/4.go
@@ -1,23 +1,23 @@
-package main
+package hot100
 
 /*
 4-移动零
 */
-func moveZeroes(nums []int)  {
+func moveZeroes(nums []int) {
 	// 双指针，将前面为零和后面不为零的互换；注意非零元素相对顺序不能动
-	i, j := 0, 0  // i为零所在下标，j为非零所在下标
-	for i < len(nums) && nums[i] != 0{
+	i, j := 0, 0 // i为零所在下标，j为非零所在下标
+	for i < len(nums) && nums[i] != 0 {
 		i++
 	}
-	j = i+1 // j一定在i之后
+	j = i + 1 // j一定在i之后
 	for j < len(nums) {
-		for j < len(nums) && nums[j] == 0{
+		for j < len(nums) && nums[j] == 0 {
 			j++
 		}
-		for i < len(nums) && nums[i] != 0{
+		for i < len(nums) && nums[i] != 0 {
 			i++
 		}
-		if i < j && j < len(nums){
+		if i < j && j < len(nums) {
 			nums[i], nums[j] = nums[j], nums[i]
 			i++
 			j++
---
+++ Hot100/5.go
@@ -0,0 +1,35 @@
+package hot100
+
+/*
+5-盛更多的水
+双指针和贪心
+*/
+func maxArea(height []int) int {
+	// 双指针,贪心只移动小的一边
+	left, right := 0, len(height)-1
+	var res int
+	for left < right {
+		small := min(height[left], height[right])
+		res = max(res, small*(right-left))
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
