a80ada5c5fd71cd14c44295fb4d87966d494935d
andelu <andelu1996@163.com>
2025-04-04 14:53:46 +0800

    feat(hot100): add 7-接雨水

---
+++ Hot100/7.go
@@ -0,0 +1,30 @@
+package hot100
+
+/*
+7-接雨水
+双指针理解有一点抽象，需要好好想和看梳理关系
+*/
+func trap(height []int) int {
+	// 从整体左右上来看
+	left, right, leftMax, rightMax := 0, len(height)-1, 0, 0
+	var ans int
+	for left < right {
+		leftMax = max(leftMax, height[left])
+		rightMax = max(rightMax, height[right])
+		if height[left] < height[right] {
+			ans += leftMax - height[left]
+			left++
+		} else {
+			ans += rightMax - height[right]
+			right--
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
