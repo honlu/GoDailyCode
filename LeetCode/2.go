18f942f425b7462d4ccc146ec62942f552fc4eea
andelu <andelu1996@163.com>
2025-07-06 15:37:30 +0800

    perf(leetcode):2-两数之和

--- LeetCode/2.go
+++ LeetCode/2.go
@@ -17,41 +17,92 @@ answer:
  *     Next *ListNode
  * }
  */
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	l3 := l1   // 在l1指向的链表上进行更改，用来保存结果链表
-	head := l3 // head最终返回的
-	flag := 0  // 进位标志
-	for l1 != nil && l2 != nil {
-		temp := l1.Val + l2.Val + flag
-		if temp >= 10 {
-			temp %= 10
-			flag = 1
-		} else {
-			flag = 0
-		}
-		l1.Val = temp
-		l2 = l2.Next
-		l3 = l1
-		l1 = l1.Next
+// func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	l3 := l1   // 在l1指向的链表上进行更改，用来保存结果链表
+// 	head := l3 // head最终返回的
+// 	flag := 0  // 进位标志
+// 	for l1 != nil && l2 != nil {
+// 		temp := l1.Val + l2.Val + flag
+// 		if temp >= 10 {
+// 			temp %= 10
+// 			flag = 1
+// 		} else {
+// 			flag = 0
+// 		}
+// 		l1.Val = temp
+// 		l2 = l2.Next
+// 		l3 = l1
+// 		l1 = l1.Next
 
-	}
-	if l2 != nil {
-		l3.Next = l2
-	}
-	for l3.Next != nil {
-		l3 = l3.Next // 注意这里和上面的位置不同
-		temp := l3.Val + flag
-		if temp >= 10 {
-			temp %= 10
-			flag = 1
-		} else {
-			flag = 0
+// 	}
+// 	if l2 != nil {
+// 		l3.Next = l2
+// 	}
+// 	for l3.Next != nil {
+// 		l3 = l3.Next // 注意这里和上面的位置不同
+// 		temp := l3.Val + flag
+// 		if temp >= 10 {
+// 			temp %= 10
+// 			flag = 1
+// 		} else {
+// 			flag = 0
+// 		}
+// 		l3.Val = temp
+// 	}
+// 	if flag == 1 {
+// 		temp := &ListNode{1, nil}
+// 		l3.Next = temp
+// 	}
+// 	return head
+// }
+
+/*
+思路：设置结果新链表的虚拟头节点
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	// 链表逆序存储数字，结果先搞一个虚拟头指针
+	head := new(ListNode)
+	p := head
+	var flag int
+	for l1 != nil && l2 != nil {
+		sum := l1.Val + l2.Val + flag
+		flag = sum / 10
+		p.Next = &ListNode{
+			Val: sum % 10,
 		}
-		l3.Val = temp
+		p = p.Next
+		l1 = l1.Next
+		l2 = l2.Next
 	}
-	if flag == 1 {
-		temp := &ListNode{1, nil}
-		l3.Next = temp
+	for l1 != nil {
+		sum := l1.Val + flag
+		flag = sum / 10
+		p.Next = &ListNode{
+			Val: sum % 10,
+		}
+		p = p.Next
+		l1 = l1.Next
 	}
-	return head
+	for l2 != nil {
+		sum := l2.Val + flag
+		flag = sum / 10
+		p.Next = &ListNode{
+			Val: sum % 10,
+		}
+		p = p.Next
+		l2 = l2.Next
+	}
+	if flag != 0 {
+		p.Next = &ListNode{
+			Val: flag,
+		}
+	}
+	return head.Next
 }
