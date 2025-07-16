d50d2a479960c0d62cd2bfae28abb3b35d333a2d
andelu <andelu1996@163.com>
2025-07-16 20:59:22 +0800

    perf(leetcode):13-罗马数字转整数

--- LeetCode/13.go
+++ LeetCode/13.go
@@ -17,17 +17,23 @@ func romanToInt(s string) int {
 		'D': 500,
 		'M': 1000,
 	}
-	// 字符串从后往前遍历，组成个位、十位、百位、千位等
+	// // 字符串从后往前遍历，组成个位、十位、百位、千位等
+	// var res int
+	// var last int
+	// for i := len(s) - 1; i >= 0; i-- {
+	// 	temp := hash[s[i]]
+	// 	if temp < last { // 若左边元素比右边小，则减
+	// 		res -= temp
+	// 	} else { // 若左边元素小于等于右边，则加
+	// 		res += temp
+	// 	}
+	// 	last = temp
+	// }
+
+	// 从前后遍历，后面比当前大就减，比当前小或等于就+
 	var res int
-	var last int
-	for i := len(s) - 1; i >= 0; i-- {
-		temp := hash[s[i]]
-		if temp < last { // 若左边元素比右边小，则减
-			res -= temp
-		} else { // 若左边元素小于等于右边，则加
-			res += temp
-		}
-		last = temp
+	for i := 0; i < len(s); i++ {
+
 	}
 	return res
 }
