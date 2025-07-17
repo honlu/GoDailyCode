3cd985ee03aa544ee28576d6e7d62696eb1db51e
andelu <andelu1996@163.com>
2025-07-17 21:53:13 +0800

    perf(leetcode):14-最长公共前缀

--- LeetCode/14.go
+++ LeetCode/14.go
@@ -8,26 +8,44 @@ question:
 answer:
 	遍历
 */
+// func longestCommonPrefix(strs []string) string {
+// 	if len(strs) == 1 { // 特殊情况 ["a"]
+// 		return strs[0]
+// 	}
+// 	var res string
+// 	temp := 0
+// 	for i := 0; i < len(strs[0]); i++ {
+// 		for j := 1; j < len(strs); j++ {
+// 			if i < len(strs[j]) && strs[j][i] == strs[0][i] {
+// 				if j == len(strs)-1 { // 特殊情况 ["flower","flower","flower","flower"]
+// 					temp = i + 1
+// 				}
+// 				continue
+// 			} else {
+// 				temp = i
+// 				goto Loop
+// 			}
+// 		}
+// 	}
+// Loop:
+// 	res = strs[0][:temp]
+// 	return res
+// }
+
+/*
+以第一个元素为主，遍历
+*/
 func longestCommonPrefix(strs []string) string {
-	if len(strs) == 1 { // 特殊情况 ["a"]
-		return strs[0]
-	}
-	var res string
-	temp := 0
+	m := len(strs)
+	var res []byte
 	for i := 0; i < len(strs[0]); i++ {
-		for j := 1; j < len(strs); j++ {
-			if i < len(strs[j]) && strs[j][i] == strs[0][i] {
-				if j == len(strs)-1 { // 特殊情况 ["flower","flower","flower","flower"]
-					temp = i + 1
-				}
-				continue
-			} else {
-				temp = i
-				goto Loop
+		temp := strs[0][i]
+		for j := 1; j < m; j++ {
+			if i >= len(strs[j]) || strs[j][i] != temp {
+				return string(res)
 			}
 		}
+		res = append(res, temp)
 	}
-Loop:
-	res = strs[0][:temp]
-	return res
+	return string(res)
 }
