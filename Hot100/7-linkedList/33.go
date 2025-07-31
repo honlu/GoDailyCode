6ff52b8d1fe3bdbd6f69893203f77d211d3807f5
andelu <andelu1996@163.com>
2025-07-31 22:28:22 +0800

    feat:排序链表

---
+++ Hot100/7-linkedList/33.go
@@ -0,0 +1,43 @@
+package hot100
+
+import "sort"
+
+/*
+33-排序链表
+https://leetcode.cn/problems/sort-list/description/?envType=study-plan-v2&envId=top-100-liked
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/*
+ 一般sort就可以解决。但是面试官可能不会让你这样做🤔
+*/
+func sortList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	// 两次for循环
+	var temp []int
+	p := head
+	for p != nil {
+		temp = append(temp, p.Val)
+		p = p.Next
+	}
+	sort.Ints(temp)
+	res := &ListNode{
+		Val: temp[0],
+	}
+	p = res
+	for i := 1; i < len(temp); i++ {
+		temp := &ListNode{
+			Val: temp[i],
+		}
+		p.Next = temp
+		p = p.Next
+	}
+	return res
+}
--- Hot100/7-linkedList/33_undo.go
+++
@@ -1,6 +0,0 @@
-package hot100
-
-/*
-33-排序链表
-https://leetcode.cn/problems/sort-list/description/?envType=study-plan-v2&envId=top-100-liked
-*/
