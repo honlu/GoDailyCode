dcdadf13aa1f12876afd46669cc5b7fd1ba8f1cf
andelu <andelu1996@163.com>
2025-08-04 20:55:08 +0800

    feat(hot100):80-70:爬楼梯

---
+++ Hot100/15-dynamicProgramming/81.go
@@ -0,0 +1,15 @@
+package dynamicprogramming
+
+/*
+80-70：爬楼梯
+*/
+func climbStairs(n int) int {
+	// 规律公式：f(n) = f(n-1) + f(n-2)
+	res := make([]int, n+1)
+	res[0] = 1
+	res[1] = 1
+	for i := 2; i < n+1; i++ {
+		res[i] = res[i-1] + res[i-2]
+	}
+	return res[n]
+}
