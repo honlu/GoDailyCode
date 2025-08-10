addac9f63c3d2bf7561922a3c2fc578c603c775d
andelu <andelu1996@163.com>
2025-08-10 14:33:26 +0800

    update: 86_undo

--- Hot100/15-dynamicProgramming/86_undo.go
+++ Hot100/15-dynamicProgramming/86_undo.go
@@ -1,1 +1,15 @@
 package dynamicprogramming
+
+/*
+86-139.单词拆分
+https://leetcode.cn/problems/word-break/?envType=study-plan-v2&envId=top-100-liked
+*/
+/*
+1. 字符串有多长
+2. 每个字段中单词有多少，需要多少个不同长度的单词才能拼接成字符串长度，
+	另外拼接成和字符串长度一样、是否字符串就一样呢？
+*/
+func wordBreak(s string, wordDict []string) bool {
+	// todo
+	return false
+}
