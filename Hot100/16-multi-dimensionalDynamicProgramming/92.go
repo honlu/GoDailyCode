9bb0c7dafa64ef999aed54aa8c3e25e1179949ea
andelu <andelu1996@163.com>
2025-09-06 13:17:32 +0800

    feat(hot100):92-64.最小路径和

---
+++ Hot100/16-multi-dimensionalDynamicProgramming/92.go
@@ -0,0 +1,40 @@
+package multidimensionaldynamicprogramming
+
+/*
+92-64.最小路径和
+https://leetcode.cn/problems/minimum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked
+*/
+
+/*
+和62相似的动态规划
+*/
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- Hot100/16-multi-dimensionalDynamicProgramming/92_.go
+++
@@ -1,9 +0,0 @@
-package multidimensionaldynamicprogramming
-
-/*
-92-64.最小路径和
-https://leetcode.cn/problems/minimum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked
-*/
-func minPathSum(grid [][]int) int {
-
-}
