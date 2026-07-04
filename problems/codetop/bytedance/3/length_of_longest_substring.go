e478896dabef8b9eb743c3e74bd76e9c0f5e236f
andelu <andelu1996@163.com>
2026-07-04 23:08:01 +0800

    Feat(bytedance): 3-无重复字符的最长子串

--- problems/codetop/bytedance/3/length_of_longest_substring.go
+++ problems/codetop/bytedance/3/length_of_longest_substring.go
@@ -1,3 +1,34 @@
-package bytedance3
+package main
 
-func lengthOfLongestSubstring(s string) int {}
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	var res int
+	var left, right int
+	subMap := make(map[byte]int)
+	for left, right = 0, 0; right < len(s); {
+		key := s[right]
+		index, ok := subMap[key]
+		if !ok {
+			subMap[key] = right
+			right++
+			if res < len(subMap) {
+				res = len(subMap)
+			}
+			continue
+		}
+		for ; left <= index; left++ {
+			delete(subMap, s[left])
+		}
+		subMap[s[right]] = right
+		right++
+
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring("bbbbb"))
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+}
